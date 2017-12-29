9180912fbc32563c6b98e88121361daf960df7c3
JNandhini <32409446+JNandhini@users.noreply.github.com>
2017-12-29 18:19:47 +0530

    Update queueclas.cpp

--- airport/queueclas.cpp
+++ airport/queueclas.cpp
@@ -1,26 +1,109 @@
 #include "Queueclas.h"
-　
-　
+
+#include<iostream>
+
+using namespace std;
+
 void Queueclas::push(Request req)
+
 {
- landing.push_back(req);
+
+	if (req.state == "landing")
+
+	{
+
+		landing.push_back(req);
+
+	}
+
+	if (req.state == "takeoff")
+
+	{
+
+		takeoff.push_back(req);
+
+	}
+
 }
-void Queueclas::
+
+
 void Queueclas::pop()
+
 {
- landing.pop_back();
+
+	if (!landing.empty())
+
+	{
+
+		landing.erase(landing.begin());
+
+	}
+
+	if (!takeoff.empty())
+
+	{
+
+		takeoff.erase(takeoff.begin());
+
+	}
+
 }
+
+void Queueclas::display()
+
+{
+
+	if (!landing.empty())
+
+	{
+
+		for (iterate = 0; iterate < landing.size(); iterate++)
+
+		{
+
+			cout << landing[iterate].get_flightid() << endl;
+
+			cout << landing[iterate].get_flightstate() << endl;
+
+			cout << landing[iterate].get_flighttime() << endl;
+
+		}
+
+	}
+
+	if (!takeoff.empty())
+
+	{
+
+		for (iterate = 0; iterate < takeoff.size(); iterate++)
+
+		{
+
+			cout << takeoff[iterate].get_flightid() << endl;
+
+			cout << takeoff[iterate].get_flightstate() << endl;
+
+			cout << takeoff[iterate].get_flighttime() << endl;
+
+		}
+
+	}
+
+}
+
 bool Queueclas::isempty()
+
 {
- if (landing.empty())
- {
-  cout << "queue is empty";
- }
+
+	if (landing.empty())
+
+	{
+
+		cout << "queue is empty";
+
+		return 0;
+
+	}
+
 }
-Queueclas::Queueclas()
-{
-}
-　
-Queueclas::~Queueclas()
-{
-}
+    
