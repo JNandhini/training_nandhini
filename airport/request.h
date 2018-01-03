3e71ce65fa040f25f6edd8094dbb2d32e019345d
JNandhini <32409446+JNandhini@users.noreply.github.com>
2018-01-03 16:25:07 +0530

    Update request.h

--- airport/request.h
+++ airport/request.h
@@ -9,19 +9,12 @@ using namespace std;
 class Request
 {
 public:
-	Airport airport;
-	time_t now;
-	time_t StartTime;
-	tm *ltm = localtime(&now);
+	
 	int FlightId = 0;
-	int hour = 0;
-	int minutes = 0;
-	int time=0;
 	string state;
+	int FlightTime=0;
 	string FlightName;
-	int seconds = 0;
-	int count = 0;
-public:
+	public:
 	void set_flightid(int FlightId);
 	int get_flightid();
 	void set_flightstate(string state);
@@ -30,7 +23,6 @@ public:
 	int get_flighttime();
 	void set_flightname(string FlightName);
 	string get_flightname(); 
-	void request_generation();
 	Request();
 	~Request();
 };
