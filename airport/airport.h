5c85f7d99e075825809bee5e078a3c08c62c62de
JNandhini <32409446+JNandhini@users.noreply.github.com>
2018-01-03 16:13:19 +0530

    Update airport.h

--- airport/airport.h
+++ airport/airport.h
@@ -1,28 +1,74 @@
 #pragma once
+
 #include<iostream>
+
 #include<vector>
+
 #include<string>
+
+#include<thread>
+
+#include<ctime>
+
+#include<future>
+
 #include"Request.h"
-#include"Queueclas.h"
-class Airport
+
+#include"queueclas.h"
+
+class airport
+
 {
+
+
+　
+
 public:
-	Request request; 
-	Queueclas queue;
+
+	Request request;
+
+	queueclas queue;
+
+public:
+
 	int iterator = 0;
+
 	int iterate = 0;
-	bool runway1 = 0;
+
+	int count = 0;
+
+	double difference1 = 0;
+
+	double ttime = 0;
+
+	double difference = 0;
+
+	int runway1 = 0;
+
 	int runway2 = 0;
-	bool busy=true;
+
+	bool busy = true;
+
 	bool idle = false;
+
+	int FlightTime = 0;
+
+	//int StartTime = 0;
+
+	string first[1];
+
+	string second[4];
+
+	int state1 = 0;
+
 	string AirportName;
-	vector<Request> landing;
-	vector<Request> takeoff;
-	
-public:
+
 	void get_details();
-	void allocation_of_runway();
+
+	int allocation_of_runway();
+
 	void request_generation();
-         Airport();
-	~Airport();
+
+
 };
+
