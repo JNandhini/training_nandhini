04097386587b13b7d370dc1ac64f06e32026fd47
JNandhini <32409446+JNandhini@users.noreply.github.com>
2018-01-03 13:01:49 +0530

    Update queue.h

--- airport/queue.h
+++ airport/queue.h
@@ -7,13 +7,11 @@ using namespace std;
 class Queueclas
 {
 public:
-	vector<Request> reques;
 	vector<Request> landing;
 	vector<Request> takeoff;
 public:
 	void push(Request reque);
 	void pop();
-	bool isempty();
 	Queueclas();
 	~Queueclas();
 };
