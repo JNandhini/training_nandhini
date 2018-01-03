d0151a073e25fc04e5bcd9134293d56a2d415373
JNandhini <32409446+JNandhini@users.noreply.github.com>
2018-01-03 18:59:26 +0530

    Update queue.h

--- airport/queue.h
+++ airport/queue.h
@@ -2,18 +2,21 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include"airport.h"
+#include"Request.h"
 using namespace std;
+
+//stores the request in queue
+
 class queueclas
 {
 public:
-	int iterate=0;
 	vector<Request> landing;
 	vector<Request> takeoff;
+	int iterate = 0;
+	int iterator = 0;
 public:
 	void push(Request reque);
 	void pop();
 	void display();
-	Queueclas();
-	~Queueclas();
+	
 };
