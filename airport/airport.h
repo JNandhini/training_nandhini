ac5e1c54add6540c1477e9bcfb9ec48103665e5f
JNandhini <32409446+JNandhini@users.noreply.github.com>
2018-01-03 13:05:03 +0530

    Update airport.h

--- airport/airport.h
+++ airport/airport.h
@@ -16,16 +16,13 @@ public:
 	bool busy=true;
 	bool idle = false;
 	string AirportName;
-	string FlightName;
-	vector<Request> requet;
 	vector<Request> landing;
 	vector<Request> takeoff;
 	
 public:
 	void get_details();
-	void allocation_of_runway(int Flightid,string state,int tim);
-	void checking_for_runway_availablity();
+	void allocation_of_runway();
 	void request_generation();
-	Airport();
+         Airport();
 	~Airport();
 };
